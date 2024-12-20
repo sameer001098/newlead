737da9b17990ae5e9841bc2a310c898d673fa8df
sameer001098 <sameershahid7032@gmail.com>
2024-12-20 14:49:15 -0300

    pportchange

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
