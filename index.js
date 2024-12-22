4c1a67d0ee5d4358c0ddec478b65e4b861a8f563
sameer001098 <sameershahid7032@gmail.com>
2024-12-22 21:01:48 +0500

    indexjschanges

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 
-// app.listen(PORT, '0.0.0.0', () => {
-//     console.log(`Server is running on http://0.0.0.0:${PORT}`);
-// });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
